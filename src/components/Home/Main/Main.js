2c8db0a0839c717a741f4f99c3e88fe960711b48
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-29 00:58:37 +0600

    set up enviorment

---
+++ src/components/Home/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <section className="slider">
+        this is slider
+      </section>
+    </main>
+  );
+};
+
+export default Main;
